b4447a437645d5fc6f6e1ae4612449a51015abe2
pifei <pifeiccc@163.com>
2019-03-29 13:56:39 +0800

    build

---
+++ build/precache-manifest.7fdde0d95e2f9e4a325cb5dbd9bcf1b3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9eb600ee07a27cdad64f",
+    "url": "./static/js/runtime~main.9eb600ee.js"
+  },
+  {
+    "revision": "17d1ea531c9c25d601c6",
+    "url": "./static/js/main.17d1ea53.chunk.js"
+  },
+  {
+    "revision": "9a177c68d49bb2272c7f",
+    "url": "./static/js/2.9a177c68.chunk.js"
+  },
+  {
+    "revision": "17d1ea531c9c25d601c6",
+    "url": "./static/css/main.0dc74864.chunk.css"
+  },
+  {
+    "revision": "51b5b82e94de12175639017a233a759c",
+    "url": "./index.html"
+  }
+];
